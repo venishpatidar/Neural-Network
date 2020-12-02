016eb4e75ccf424bca64a638c1e40c2dec9b6bf6
Venish Patidar <venishpatidar@gmail.com>
2020-12-02 21:47:20 +0530

    Added McCulloch-Pitts Neuron Model

---
+++ Artificial Neural Network Basic Ideas and Concepts/McCulloch-Pitts Neuron Model.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Creating neuron function 
+def neuron(inputs,weights,threshold):
+    # calculating weights 
+    z = (inputs*weights)
+    z = np.sum(z)
+
+    output = 0
+    # Condition for firing the neuron if summation is over threshold
+    if z>threshold:
+        output=1
+    else:
+        output=0
+    return output
+
+# intializing inputs, weights and threshold
+inputs = np.array([1,2,3])
+weights = np.array([-1,-5,6])
+threshold = 10
+
+# firng neuron
+print(neuron(inputs,weights,threshold))
